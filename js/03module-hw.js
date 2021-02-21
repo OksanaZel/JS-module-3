1afc54d247df5bd8bdf9a52b0c9dc66ef39f54eb
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-02-21 15:56:00 +0200

    Добавляет задачи из 3 модуля

--- js/03module-hw.js
+++ js/03module-hw.js
@@ -1,1 +1,20 @@
-// 1.
+// 1.Присвой переменной apartment объект описывающий квартиру со 
+//следующими характеристиками:
+//imgUrl - фотография, значение 'https://via.placeholder.com/640x480';
+//descr - описание, значение 'Просторная квартира в центре';
+//rating - рейтинг, значение 4;
+//price - цена, значение 2153;
+//tags - метаинформация, массив ['premium', 'promoted', 'top'].
+
+// const apartment = {
+// imgUrl: 'https://via.placeholder.com/640x480',
+// descr: 'Просторная квартира в центре',
+// rating: 4,
+// price: 2153,
+// tags: ['premium', 'promoted', 'top'],
+// }
+
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
